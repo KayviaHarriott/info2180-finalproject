fd13104ac837d74552ea59b6a52788c36cc09db8
Jason Hunter <jahunter767@gmail.com>
2021-12-03 11:03:16 -0500

    added the create issue screen

--- newIssueView.php
+++ newIssueView.php
@@ -1,1 +1,30 @@
-<h1>NEW ISSUE</h1>
+<form id="new-issue-form" action="#">
+<label id="i-title-label" for="i-title">Title</label><br>
+<input type="text" class="i-title-input" id="i-title" placeholder=""/><br><br>
+
+<label id="i-desc-label" for="i-desc">Description</label><br>
+<input type="text" class="i-desc-input" id="i-desc" placeholder=""/><br><br>
+
+<label id="i-assign-label" for="i-assign">Assigned To</label><br>
+<select id="i-assign" class="i-assign-input" placeholder="1">
+<?php foreach ([1, 2, 3, 4] as $i): ?>
+    <option value=<?= $i ?>></option>
+<?php endforeach;?>
+</select><br><br>
+
+<label id="i-type-label" for="i-type">Type</label><br>
+<select id="i-type" class="i-type-input">
+<?php foreach ([1, 2, 3, 4] as $i): ?>
+    <option value=<?= $i ?>></option>
+<?php endforeach;?>
+</select><br><br>
+
+<label id="i-priority-label" for="i-priority">Priority</label><br>
+<select id="i-priority" class="i-priority-input">
+<?php foreach ([1, 2, 3, 4] as $i): ?>
+    <option value=<?= $i ?>></option>
+<?php endforeach;?>
+</select><br><br>
+
+<input type="submit"  class="new-issue-button" id="new-issue-button" value="Submit"/>
+</form>
--- newUserView.php
+++ newUserView.php
@@ -1,19 +1,15 @@
 <form id="new-user-form" action="#">
-<label id="f-name-label" for="f-name">Firstname</label>
-<br>
-<input type="text" class="f-name-input" id="f-name" placeholder=""/>
-<br><br>
-<label id="l-name-label" for="l-name">Lastname</label>
-<br>
-<input type="text" class="l-name-input" id="l-name" placeholder=""/>
-<br><br>
-<label id="p-word-label" for="p-word">Password</label>
-<br>
-<input type="text" class="p-word-input" id="p-word" placeholder=""/>
-<br><br>
-<label id="email-label" for="email">Email</label>
-<br>
-<input type="text" class="email-input" id="email" placeholder=""/>
-<br><br>
+<label id="f-name-label" for="f-name">Firstname</label><br>
+<input type="text" class="f-name-input" id="f-name" placeholder=""/><br><br>
+
+<label id="l-name-label" for="l-name">Lastname</label><br>
+<input type="text" class="l-name-input" id="l-name" placeholder=""/><br><br>
+
+<label id="p-word-label" for="p-word">Password</label><br>
+<input type="text" class="p-word-input" id="p-word" placeholder=""/><br><br>
+
+<label id="email-label" for="email">Email</label><br>
+<input type="text" class="email-input" id="email" placeholder=""/><br><br>
+
 <input type="submit"  class="new-user-button" id="new-user-button" value="Submit"/>
 </form>
